c1c5bdcb7f91bcd0061328ae1dbfa3dfb34e8885
leticiakalila <leticiakaliladarlene@gmail.com>
2023-09-06 15:41:44 +0700

    memperbaharui repositori github

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns = [
+    path('main/', include('main.urls')),
 ]
